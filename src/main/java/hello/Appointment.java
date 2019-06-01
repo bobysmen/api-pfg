3d79ec310d2fb3b318cf28fedfc31d257556c9b3
bobysmen <elhaker_1989@hotmail.com>
2019-06-01 20:59:16 +0200

    TODO Appointment

---
+++ src/main/java/hello/Appointment.java
@@ -0,0 +1,18 @@
+package hello;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="appointment")
+public class Appointment {
+
+	@EmbeddedId
+	private AppointmentId appointmentId;
+	
+}
---
+++ src/main/java/hello/PatientRepository.java
@@ -0,0 +1,7 @@
+package hello;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PatientRepository extends CrudRepository<Patient, Integer> {
+
+}
