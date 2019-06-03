c05f56360af9b04899472148776bb1904a14b870
bobysmen <elhaker_1989@hotmail.com>
2019-06-03 20:50:27 +0200

    test all patient

--- src/main/java/hello/PatientController.java
+++ src/main/java/hello/PatientController.java
@@ -1,8 +1,14 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(path="/patient")
@@ -10,5 +16,27 @@ public class PatientController {
 	
 	@Autowired
 	private PatientRepository patientRepository;
+	
+	@PostMapping("/add")
+	public @ResponseBody Patient add(@RequestBody Patient patient) {
+		Patient pat = null;
+		
+		if(!patientRepository.findById(patient.getId()).isPresent()) {
+			pat = patient;
+			patientRepository.save(pat);
+		}
+		
+		return pat;
+	}
+	
+	@PostMapping("/all")
+	public @ResponseBody Iterable<Patient> all () {
+
+		Iterable<Patient> list = new ArrayList<>();
+		
+		list = patientRepository.findAll();
+		
+		return list;
+}
 
 }
