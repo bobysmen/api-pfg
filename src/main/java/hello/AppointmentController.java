a818fe7776d9e344930f54a0763fd120525d25ac
bobysmen <elhaker_1989@hotmail.com>
2019-06-06 23:47:48 +0200

    Appointment find by date

--- src/main/java/hello/AppointmentController.java
+++ src/main/java/hello/AppointmentController.java
@@ -1,8 +1,14 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(path="/appointment")
@@ -11,4 +17,16 @@ public class AppointmentController {
 	
 	@Autowired
 	private AppointmentRepository appointmentRepository;
+	
+	
+	@PostMapping("/today")
+	public @ResponseBody Iterable<Appointment>add(Date dateToday) {
+		
+		Iterable<Appointment> list = new ArrayList<>();
+		
+		list = appointmentRepository.findAllByDate(dateToday);
+		
+		return list;
+	}
+	
 }
--- src/main/java/hello/AppointmentRepository.java
+++ src/main/java/hello/AppointmentRepository.java
@@ -1,7 +1,11 @@
 package hello;
 
+import java.util.Date;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
 
+	public Iterable<Appointment> findAllByDate(Date dateToday);
+	
 }
